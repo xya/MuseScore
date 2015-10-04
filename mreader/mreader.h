b03647dc09a26a3888284924eff78ecb9b9c4632
Pierre-Andre Saulais <pasaulais@free.fr>
2015-10-04 18:22:05 +0100

    Add license

--- mreader/mreader.h
+++ mreader/mreader.h
@@ -1,3 +1,22 @@
+//=============================================================================
+//  MuseScore Reader
+//
+//  Copyright (C) 2011 Werner Schweer and others
+//  Copyright (C) 2015 Pierre-Andre Saulais
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License version 2.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//=============================================================================
+
 #ifndef MSCORE_MREADER_MREADER_H
 #define MSCORE_MREADER_MREADER_H
 
